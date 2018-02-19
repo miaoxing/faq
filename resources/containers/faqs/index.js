e6c6829bd26caa017c8783504512b28d223a931c
twinh <twinhuang@qq.com>
2018-02-19 21:56:08 +0800

    更新模板

--- resources/containers/faqs/index.js
+++ resources/containers/faqs/index.js
@@ -1,18 +1,18 @@
-import template from 'lodash.template';
+import faqItem from '../../templates/faq-item.ejs';
 
 export default class FaqIndex {
   render() {
-    var $list = $('.js-faq-list').list({
+    const $list = $('.js-faq-list').list({
       url: $.url('faqs.json'),
-      template: template($('.js-faq-item-tpl').html()),
+      template: faqItem,
       localData: wei.faqs
     });
 
     $list.on('click', '.js-faq-link', function () {
-      var $item = $(this).closest('.js-faq-item');
+      const $item = $(this).closest('.js-faq-item');
       $item.find('.js-faq-angel').toggleClass('answer-open');
 
-      var $answer = $item.find('.js-faq-answer');
+      const $answer = $item.find('.js-faq-answer');
       $answer.toggle();
       if ($answer.is(':visible')) {
         $.post($.url('faqs/%s/view.json', $(this).data('id')));
