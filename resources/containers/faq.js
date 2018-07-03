89480baea9267e1f899efc5ba774a132d5a63c60
twinh <twinhuang@qq.com>
2018-07-03 10:20:05 +0800

    feat: 先还原faq的更改

--- resources/containers/faq.js
+++ resources/containers/faq.js
@@ -1,22 +1,3 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {AppContainer} from 'react-hot-loader'
+import container from 'container';
 
-import Root from './containers.js';
-
-const render = Component => {
-  ReactDOM.render(
-    <AppContainer>
-      <Component/>
-    </AppContainer>,
-    document.getElementById('root')
-  )
-}
-
-render(Root)
-
-// if (module.hot) {
-//   module.hot.accept(() => {
-//     render(Root)
-//   })
-// }
+container(require.context('.', true, /^(?!.*admin).*\.(\w+)$/));
