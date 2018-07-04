7f3ade135cc33ead86c2348c9730ea9382c6e07e
twinh <twinhuang@qq.com>
2018-07-04 08:58:13 +0800

    fix: 还原faq的js文件

--- resources/containers/faqs/index.js
+++ resources/containers/faqs/index.js
@@ -1,30 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import styled from 'styled-components';
-import Plugin from 'components/Plugin.jsx';
-import '../faq.scss';
+import faqItem from '../../templates/faq-item.ejs';
 
-const Answer = styled.div`
-  img {
-    max-width: 100%;
-  }
-`;
-
-export default class extends React.Component {
-  componentDidMount() {
+export default class FaqIndex {
+  render() {
     const $list = $('.js-faq-list').list({
       url: $.url('faqs.json'),
-      template: data => {
-        const container = document.createElement('div');
-        return ReactDOM.render(<li className="js-faq-item">
-          <a href="javascript:;" className="js-faq-link list-item has-feedback" data-id={data.id}>
-            <h4 className="list-heading">{data.question}</h4>
-            <i className="js-faq-angel bm-angle-right list-feedback"/>
-          </a>
-          <Answer className="js-faq-answer list-body p-b p-r text-normal display-none"
-            dangerouslySetInnerHTML={{__html: data.answer}}/>
-        </li>, container);
-      }
+      template: faqItem,
+      localData: wei.faqs
     });
 
     $list.on('click', '.js-faq-link', function () {
@@ -37,22 +18,5 @@ export default class extends React.Component {
         $.post($.url('faqs/%s/view.json', $(this).data('id')));
       }
     });
-  }
-
-  render() {
-    return <div>
-      <Plugin name="faqsIndexTop"><div>1</div></Plugin>
-      <form className="search-form" action="" method="get">
-        <div className="border-all border-radius">
-          <input className="search-input" name="q" defaultValue={$.req('q')} type="text"
-            placeholder="请输入问题或答案搜索"/>
-        </div>
-        <button className="search-submit">
-          <i className="text-muted ni ni-search"/>
-        </button>
-      </form>
-      <ul className="js-faq-list list list-indented">
-      </ul>
-    </div>
   }
 }
