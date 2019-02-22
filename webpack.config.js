1be11720f9a41fdf3bf6499e8a89bfbc6f63e76c
twinh <twinhuang@qq.com>
2019-02-22 14:27:42 +0800

    refactoring: 统一modules到resources目录中

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const WebpackConfig = require('../app/modules/webpack-config');
+const WebpackConfig = require('../app/resources/modules/webpack-config');
 
 module.exports = WebpackConfig.build({
   name: path.basename(__dirname),
