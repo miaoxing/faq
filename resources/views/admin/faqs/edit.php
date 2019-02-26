66ef0ead8003aacc3b2b122afe45036071f92d92
twinh <twinhuang@qq.com>
2019-02-26 11:42:28 +0800

    refactoring: btn-default > btn-secondary

--- resources/views/admin/faqs/edit.php
+++ resources/views/admin/faqs/edit.php
@@ -4,7 +4,7 @@ $view->layout();
 ?>
 
 <?= $block('header-actions') ?>
-<a class="btn btn-default" href="<?= $url('admin/faqs') ?>">返回列表</a>
+<a class="btn btn-secondary" href="<?= $url('admin/faqs') ?>">返回列表</a>
 <?= $block->end() ?>
 
 <div class="row">
@@ -42,7 +42,7 @@ $view->layout();
             提交
           </button>
           &nbsp; &nbsp; &nbsp;
-          <a class="btn btn-default" href="<?= $url('admin/faqs') ?>">
+          <a class="btn btn-secondary" href="<?= $url('admin/faqs') ?>">
             <i class="fa fa-undo bigger-110"></i>
             返回列表
           </a>
